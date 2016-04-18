d4fb37b5be9dcc0bb678be9f9406cf72581a5931
benho2000 <benho2000@yahoo.com>
2016-04-17 20:21:47 -0400

    Create site.0417.js

---
+++ wwwroot/js/site.0417.js
@@ -0,0 +1,118 @@
+var app = angular.module('angularTable', ['angularUtils.directives.dirPagination', 'ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.pagination', 'trNgGrid', 'dx']);
+
+app.controller('carCtrl', function ($scope, $http) {
+
+    $scope.cars = []; //declare an empty array
+    
+    $http.get("/api/cars").success(function (response) {
+        $scope.cars = response;  //ajax request to fetch data into $scope.data
+        console.log(response);
+    });
+
+    $scope.sort = function (keyname) {
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    };
+
+});
+
+app.controller('carGridController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.carGrid = {
+        enableRowSelection: true,
+        enableRowHeaderSelection: false,
+        multiSelect: true,
+        onRegisterApi: function (gridApi) {
+            $scope.gridApiNames = gridApi;
+        },
+        paginationPageSizes: [25, 50, 75],
+        paginationPageSize: 25,
+        columnDefs: [
+                        { name: 'Id' },
+                        { name: 'Make' },
+                        { name: 'Year' },
+                        { name: 'Status' }
+        ]
+    };
+    
+    $http.get('http://localhost:24099/api/cars')
+  .success(function (data) {
+      $scope.carGrid.data = data;
+  });
+}]);
+
+app.controller("trnggridCtrl", ["$scope", "$http", function ($scope, $http) {
+    $scope.disableScheduleButton = true;
+    $scope.showModal = false;
+    $scope.showChildTable = false;
+    $scope.makeDropdownOptions = ["Honda", "Toyota", "Ford", "Lexus", "Mazada", "Kia", "Jeep"];
+       
+    $scope.mySelectedItems = [];
+    //$scope.myGlobalFilter = ''; // no need to define this
+
+    function init() {
+        $http.get('http://localhost:24099/api/cars')
+            .success(function (data) {
+                $scope.myItems = data;
+                $scope.myCars = data;
+        });
+    }
+
+    $scope.$watch("myMasterGlobalFilter", function () {
+        console.log("my masater global filter: " + $scope.myGlobalFilter);
+    });
+
+    $scope.$watchCollection("mySelectedItems", function () {
+        // your logic goes here
+        console.log("num of selected: " + $scope.mySelectedItems.length);
+        console.log("data: " + JSON.stringify($scope.mySelectedItems));
+
+        if ($scope.mySelectedItems.length > 0) {
+            $scope.disableScheduleButton = false;
+            $scope.showChildTable = true;
+        }
+        else {
+            $scope.disableScheduleButton = true;
+            $scope.showChildTable = false;
+        }
+
+    });
+
+    $scope.OnScheduleClicked = function () {
+
+        //console.log(JSON.stringify($scope.mySelectedItems));
+
+        // send data back to server
+        if ($scope.mySelectedItems.length > 0)
+        {
+            console.log("data to send back: " + JSON.stringify($scope.mySelectedItems));
+        }
+    };
+
+    $scope.OnRefresh = function ($scope, $http) {
+        init();
+    };
+
+
+    $scope.OnRemoveFilter = function () {
+        $scope.myGlobalFilter = '';
+        $scope.myColumnFilter = '';
+        init();
+    };
+
+    $scope.OnUnselect = function () {
+        $scope.mySelectedItems = [];
+    };
+
+    $scope.OnItemButtonClick = function (id) {
+        console.log("id of row clicked: " + id);
+    }
+    
+    init();
+}]);
+
+app.filter("computedMakes", function () {
+    return function (fieldValueUnusedForComputedFields, dataItem) {
+        return dataItem.Make.join(",");
+    };
+});
